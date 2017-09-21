dd7ddc6522ee45e12f229d50355c3c4c483a1e07
Margaret <30538997+margaretnatalie@users.noreply.github.com>
2017-09-20 18:53:21 -0700

    figuring it out

--- starter_code/js/index.js
+++ starter_code/js/index.js
@@ -1,4 +1,42 @@
 $(document).ready(function() {
+
+
+$('.readmore').delay().show(0);
+
+	$(".readmore").click(function(){
+		$("#show-this-on-click").slideDown();
+	})
+	});
+
+$('.readmore').delay().hide(0);
+
+$(".readless").click(function(){
+		$("#show-this-on-click").slideUp();
+	})
+
+$(".readless").delay().show(0);
+
+$(".readless").click(function(){
+		$("#show-this-on-click").slideUp();
+	})
+
+$('.readmore').on('click',function(e) {
+     e.preventDefault(); 
+     e.stopPropagation(); 
+   
+   return false;  
+
+$('.readless').on('click',function(e) {
+     e.preventDefault(); 
+     e.stopPropagation(); 
+   
+   return false;        
+
+
 })
+
+});
+
+
 
 console.log('this is working');
