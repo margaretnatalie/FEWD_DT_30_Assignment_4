5e01128f699ff574d0e115c7b0c5cdd4ba71deb3
Margaret <30538997+margaretnatalie@users.noreply.github.com>
2017-09-23 10:32:49 -0700

    changes

--- starter_code/js/index.js
+++ starter_code/js/index.js
@@ -1,42 +1,47 @@
 $(document).ready(function() {
 
-
-$('.readmore').delay().show(0);
-
-	$(".readmore").click(function(){
+$(".readmore").click(function(){
 		$("#show-this-on-click").slideDown();
 	})
+$('.readmore').on('click', function(){
+	$('.readless').show()
+	$('.readmore').hide();
+	})
+
+$(".readless").on('click', function(event){
+	$("#show-this-on-click").slideUp("slow");
+	$("#show-this-on-click").hide();
+	$(".readless").hide();
+	$('.readmore').show();
+	event.preventDefault();
+
+})
+
+$('.learnmore').click(function(){
+	$("#learnmoretext").slideDown();
+	$('.learnmore').hide();
+	event.preventDefault();
+})
 	});
-
-$('.readmore').delay().hide(0);
-
-$(".readless").click(function(){
-		$("#show-this-on-click").slideUp();
-	})
-
-$(".readless").delay().show(0);
-
-$(".readless").click(function(){
-		$("#show-this-on-click").slideUp();
-	})
 
 $('.readmore').on('click',function(e) {
      e.preventDefault(); 
      e.stopPropagation(); 
-   
-   return false;  
-
-$('.readless').on('click',function(e) {
-     e.preventDefault(); 
-     e.stopPropagation(); 
-   
-   return false;        
-
-
-})
-
+  
 });
 
+//my personal notes on this assignment
 
+// $('.readmore').delay().hide(0);
 
-console.log('this is working');
+// $(".readless").delay().show(0);
+// $('.readless').on('click', '.readmore', function(){
+
+// 	})
+
+// $(".readless").click(function(){
+// 		$("#show-this-on-click").slideUp();
+// 	})
+// $(".readless").on('click',function(){
+// 		$(".readless hide").slideUp();
+	// })
